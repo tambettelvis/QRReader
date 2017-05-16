abefe74cd89978a3ba85cd118bd291a8817de79c
tambettelvis <tambetttanilsoo@gmail.com>
2017-05-16 17:20:33 +0300

    RemovePacks.java

---
+++ app/src/main/java/com/tanilsoo/tambet_telvis/qrreader/RemovePacks.java
@@ -0,0 +1,56 @@
+package com.tanilsoo.tambet_telvis.qrreader;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tambet-Telvis on 5.05.2017.
+ */
+
+public class RemovePacks extends Activity {
+
+    public static List<String> postTypes =  new ArrayList<String>();
+    List<Integer> amount =  new ArrayList<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_packs);
+        createAmountSpinner();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, postTypes);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.pack_spinner);
+        sItems.setAdapter(adapter);
+
+
+        ArrayAdapter<Integer> adapter2 = new ArrayAdapter<Integer>(
+                this, android.R.layout.simple_spinner_item, amount);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems2 = (Spinner) findViewById(R.id.amt_spinner);
+        sItems2.setAdapter(adapter2);
+
+
+    }
+
+    private void createAmountSpinner(){
+        for(int i=1; i < 100; i++){
+            amount.add(i);
+        }
+    }
+
+
+}
